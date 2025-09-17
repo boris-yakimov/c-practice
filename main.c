8321d28c9c88f99dc45b35ed38358cdf783d64af
Boris Yakimov <yakimovb@gmail.com>
2025-09-17 17:45:30 +0300

    c types and function prototypes

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
+#include "prototypes.h" // include our headers file where we store function prototypes
 // import standard I/O library
 #include <stdio.h>
 
+/*
+this type of comment is also supported
+and it is a multiline comment
+*/
 int main() {
   int age;
   age = 37;
@@ -9,17 +14,49 @@ int main() {
   printf("%d years old\n", age);
   printf("%d cm tall\n", height);
 
+  // char a = "a";           // should be an err becuase "a" is a char * not a
+  char *a = "a";           // should work
+  char single_thing = 'b'; // should work
+
+  printf("%s\n", a);
+  printf("%s\n", &single_thing);
+
+  // change value of a var
+  a = "no longer a";
+  printf("%s\n", a);
+
+  // const int x = 5;
+  // x = 10; // will give a complier error because we cannot change a constant
+
+  int x = 5;
+  x = 10; // this however will work because it is a var
+
+  float result =
+      add(7, 36); // this will be a compiler error that add is not defined
+                  // unless we create a function prortype before the main()
+                  // function, because our add function is declared after main
+  printf("%f\n", result);
+
+  // run the function that shows how void works
+  iDoNothing();
+
   return 0;
+}
+
+float add(int x, int y) {
+  return (float)(x + y); // we are type casting the result of integers x+y to
+                         // become a float
 }
 
 // function returns an int
 // in C return values are before the func name
-int helloWorld() {
+int helloWorld(void) {
   printf("Hello, world!\n"); // statements in C end with a semicolon ;
   return 0;                  // we return exit code 0 to the OS
 }
 
-/*
-this type of comment is also supported
-and it is a multiline comment
-*/
+// void in C is the absence of value
+void iDoNothing(void) {
+  printf("func that takes no values and returns no values to provide an "
+         "example for void");
+}
---
+++ prototypes.h
@@ -0,0 +1,10 @@
+// function prototypes; declare what function will be created later, in for the
+// compiler to be able to verify that correct arguments are used in function
+// call before it needs to see the full function delcaration
+float add(int x, int y);
+void iDoNothing(void);
+int helloWorld(void);
+
+// we use this file as a sort of specification of all functions that we will
+// later create, .h headers file than has to be imported with #include
+// "prototypes.h" in our main function
